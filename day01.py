dceed56a4d88edd89142d4e29dcdd06fc28c0f9e
root <root@localhost.localdomain>
2018-09-25 03:54:00 -0400

    www

---
+++ day01.py
@@ -0,0 +1,25 @@
+#c = raw_input(">>")
+#k = raw_input(">>")
+#int_c = int(c)
+#int_k = int(k)
+#area = int_c * int_k
+#print(area)
+#EP 1:
+#time_ = int(raw_input(">>"))
+#min_ = time_// 60
+#time_ =time_ % 60
+#print(min_,time_)
+#EP 2:
+#x = raw_input(">>")
+#y = raw_input(">>")
+#a = raw_input(">>")
+#b = raw_input(">>")
+#c = raw_input(">>ii
+x,y,a,b,c=1,1,0,0,0
+part1 = ( 3 + 4 * x ) / 5
+part2 = 10 * (y - 5) * (a + b + c) / x
+part3 = 9 * ((4/x)+(9+x)/y)
+num = part1 +part2 +part3
+print(num)
+
+
