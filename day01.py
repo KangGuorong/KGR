2c4eb3f8ea5be89ec0964693bf519defe3de2799
root <root@localhost.localdomain>
2018-09-25 07:58:11 -0400

    www

--- day01.py
+++ day01.py
@@ -15,11 +15,78 @@
 #a = raw_input(">>")
 #b = raw_input(">>")
 #c = raw_input(">>ii
-x,y,a,b,c=1,1,0,0,0
+'''x,y,a,b,c=1,1,0,0,0
 part1 = ( 3 + 4 * x ) / 5
 part2 = 10 * (y - 5) * (a + b + c) / x
 part3 = 9 * ((4/x)+(9+x)/y)
 num = part1 +part2 +part3
+print(num)'''
+#zy1:
+'''
+celsius = int(raw_input(">>"))
+fahrenheit = ( 9.0 / 5.0) * celsius + 32
+print(celsius)
+print(fahrenheit)'''
+#zy2:
+'''
+radius = float ( raw_input(">>"))
+length = float ( raw_input(">>"))
+area = radius * radius * 3.14
+volume = area * length
+print(area)
+print(volume)
+'''
+#zy3:
+'''
+feet= float (raw_input("Enter a value for feet:>>"))
+meters = feet * 0.305
+print(feet)
+print(meters)
+'''
+#zy4:
+'''
+m = float ( raw_input("Enter the amount of water in kilograms>>"))
+cswd = float ( raw_input("Enter the initial temperature>>"))
+zzwd = float ( raw_input("Enter the final temperature>>"))
+Q = m * (zzwd - cswd) * 4184
+print(Q)'''
+#zy5:
+'''
+ce = int ( raw_input(">>") )
+nl = float ( raw_input(">>") )
+lx = ce * (nl / 1200 )
+print(lx)
+'''
+#zy6:
+'''
+v1,v0,t = eval ( raw_input("Enter v0,v1,and t:"))
+a = (v0 - v1 ) / t
+print('The average acceleration is {:.4f}'.format(a))
+'''
+#zy7:
+'''ck = int ( raw_input(">>"))
+t1 = ck * (1+0.00417)
+t2 = (ck + t1 ) * ( 1 + 0.00417 )
+t3 = (ck + t2 ) * ( 1 + 0.00417 )
+t4 = (ck + t3 ) * ( 1 + 0.00417 )
+t5 = (ck + t4 ) * ( 1 + 0.00417 )
+t6 = (ck + t5 ) * ( 1 + 0.00417 )
+print(t6)'''
+#zy8:
+zs = int ( raw_input(">>"))
+g = zs % 10
+s = zs //10 % 10
+b = zs //10 // 10
+num = g + s + b
 print(num)
+
+
+
+
+
+
+
+
+
 
 
